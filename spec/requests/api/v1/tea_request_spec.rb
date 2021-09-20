e8570bf03f25d8d9990280c44a2563ff4341c558
Regina Casias <reginaannvigil@gmail.com>
2021-09-20 11:07:03 -0600

    Completed first test for tea can list all leas

--- spec/requests/api/v1/tea_request_spec.rb
+++ spec/requests/api/v1/tea_request_spec.rb
@@ -6,7 +6,27 @@ describe "Tea API" do
 
     get '/api/v1/teas'
 
-    binding.pry
+    # binding.pry
     expect(response).to be_successful
+    teas = JSON.parse(response.body, symbolize_names: true)
+
+    expect(teas.count).to eq(3)
+
+    teas.each do |tea|
+      expect(tea).to have_key(:id)
+      expect(tea[:id]).to be_an(Integer)
+
+      expect(tea).to have_key(:title)
+      expect(tea[:title]).to be_a(String)
+
+      expect(tea).to have_key(:description)
+      expect(tea[:description]).to be_a(String)
+
+      expect(tea).to have_key(:temperature)
+      expect(tea[:temperature]).to be_a(Integer)
+
+      expect(tea).to have_key(:brew_time)
+      expect(tea[:brew_time]).to be_a(String)
+    end
   end
 end
