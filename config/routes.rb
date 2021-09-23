cfbad57e34140609dd65b4c776c81aee7cfec4e4
Regina Casias <reginaannvigil@gmail.com>
2021-09-23 16:08:05 -0600

    Added validations

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,24 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :teas
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
+      resources :subscriptions
       resources :customers do
         resources :subscriptions, controller: :customer_subscriptions
       end
     end
   end
 
-  namespace :api do
-    namespace :v1 do
-      resources :subscriptions
-    end
-  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :customers do
+  #       resources :subscriptions, controller: :customer_subscriptions
+  #     end
+  #   end
+  # end
+  #
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :subscriptions
+  #   end
+  # end
 end
